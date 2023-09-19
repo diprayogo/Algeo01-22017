d6cd34e26ede7a69e7390e44cb08727b7c580da8
unknown <diprayogo45@gmail.com>
2023-09-20 06:37:54 +0700

    initiate dirs

---
+++ learn/HelloWorld.java
@@ -0,0 +1,35 @@
+
+import java.util.*;
+import javax.swing.*;
+
+public class HelloWorld {
+
+  public static void main(String[] args) {
+    // System.out.println("Hello World");
+
+    // Scanner input = new Scanner(System.in);
+    // System.out.print("nama: ");
+    // String namaCons = input.nextLine();
+
+    // System.out.print("usia: ");
+    // int usiaCons = input.nextInt();
+
+    // System.out.println("Nama saya " + namaCons + ", " + usiaCons + " tahun");
+
+    // int i;
+    // for (i = 1; i <= usiaCons; i++) {
+    //   System.out.println("i = " + i);
+    // }
+    
+    // deklarasi + inisialisasi by GUI input
+    String nama = JOptionPane.showInputDialog("nama: ");
+    String usia = JOptionPane.showInputDialog("usia: ");
+    int UsiaInt = Integer.parseInt(usia);
+
+    String intro = "Nama saya " + nama + ", " + UsiaInt + " tahun";
+    System.out.println(intro);
+    JOptionPane.showMessageDialog(null, intro);
+    System.exit(0);
+    
+  }
+}
---
+++ src/Main.java
@@ -0,0 +1,29 @@
+
+
+// text-based dan CLI (wajib)
+public class Main {
+  public static void main(String[] args)() {
+    menu = pilihMenu();
+    switch (menu) {
+      case 1: break;
+      case 2: break;
+      case 3: break;
+      case 4: break;
+      case 5: break;
+      case 6: break;
+      case 7: break;
+      case 8: break; // submenu 1.1
+      case 9: break; // submenu 1.2
+      case 10: break; // submenu 1.3
+      case 11: break; // submenu 1.4
+      case 12: break; // submenu 2.1 determinan reduksi baris
+      case 13: break; // submenu 2.2 determinan ekspansi kofaktor
+      case 14: break; // submenu 3.1 matriks balikan dengan gauss jordan [A|I] ~ OBE ~ [I|A^-1]
+      case 15: break; // submenu 3.2 matriks balikan dengan kofaktor adjoin determinan, cek dulu var det != 0
+    }
+  }
+
+  int pilihMenu() {
+    return 1;
+  } 
+}
